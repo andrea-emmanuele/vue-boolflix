6508f3830ec39447c3b595d3c939790e8bca93ff
andrea <git@andreaemmanuele.it>
2021-04-13 11:20:33 +0200

    Milestone 1, Milestone 2 completed and milestone 3 half-completed

--- assets/js/search.js
+++ assets/js/search.js
@@ -11,7 +11,7 @@ new Vue({
        queryResults: ""
    },
     methods: {
-       search(refs) {
+        search(refs) {
            if (!this.query) {
              this.isSearching = !this.isSearching;
 
@@ -20,19 +20,48 @@ new Vue({
                 },50);
            }
        },
-       getSearchedFilms() {
-           let uri = "https://api.themoviedb.org/3/search/movie?api_key=5809053c05dc9a515b67e3ed5d1ef360&query=" + this.query;
+        getSearchedFilms() {
+           let uri = "https://api.themoviedb.org/3/search/multi?api_key=5809053c05dc9a515b67e3ed5d1ef360&query=" + this.query;
 
            if (this.query) {
                axios
                    .get(uri)
                    .then(response => {
                        this.queryResults = response.data.results;
+
+                       console.log(this.queryResults);
                    });
            }
            else if (this.query === "") {
                this.queryResults = [];
            }
-       }
+       },
+        getTitle(result) {
+           if (result.title) {
+               return result.title;
+           }
+           else {
+               return result.name;
+           }
+        },
+        getType(type) {
+           if (type === "tv") {
+               return "TV Series";
+           }
+           else {
+               return type;
+           }
+        },
+        getLanguage(language) {
+           if (language === "en") {
+               return "gb";
+           }
+           else {
+               return language;
+           }
+       },
+        getRate(vote) {
+
+        }
     }
 });
